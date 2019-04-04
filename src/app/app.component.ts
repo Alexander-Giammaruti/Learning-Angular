5d02d0559e971cf45473f9f1b0f19dbc8eca2e97
Alexander-Giammaruti <arg010@shsu.edu>
2019-04-04 15:11:03 -0500

    [Added newest Angular Animations]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger } from '@angular/animations'
 import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('divstate', [
+      
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   loadedTab = 'recipe';
